9d351a1ca4981e89a8b366822d2a6912a8206a5f
andrew-2e128 <andrew.2e128@outlook.com>
2025-03-21 14:50:33 +0200

    Remove CMake Internal build config and enable standard CMake build configs

--- Code/GameEngine/Source/GameNetwork/ConnectionManager.cpp
+++ Code/GameEngine/Source/GameNetwork/ConnectionManager.cpp
@@ -679,7 +679,7 @@ void ConnectionManager::processChat(NetChatCommandMsg *msg)
 
 void ConnectionManager::processFile(NetFileCommandMsg *msg) 
 {
-#ifdef _INTERNAL
+#ifdef _DEBUG
 	UnicodeString log;
 	log.format(L"Saw file transfer: '%hs' of %d bytes from %d", msg->getPortableFilename().str(), msg->getFileLength(), msg->getPlayerID());
 	DEBUG_LOG(("%ls\n", log.str()));
